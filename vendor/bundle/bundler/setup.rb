086b69ab79db610f91e40d55065302d048674551
Dennis Paagman <dennispaagman@gmail.com>
2019-05-07 09:56:23 +0200

    Update to secure version of Alfredo

--- vendor/bundle/bundler/setup.rb
+++ vendor/bundle/bundler/setup.rb
@@ -3,7 +3,8 @@ require 'rbconfig'
 ruby_engine = defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'
 ruby_version = RbConfig::CONFIG["ruby_version"]
 path = File.expand_path('..', __FILE__)
-$:.unshift File.expand_path("#{path}/../#{ruby_engine}/#{ruby_version}/gems/mini_portile-0.6.2/lib")
-$:.unshift File.expand_path("#{path}/../#{ruby_engine}/#{ruby_version}/gems/nokogiri-1.6.6.2/Users/Dennis/Dropbox/Alfred.alfredpreferences/workflows/user.workflow.A203831F-4005-49BB-AD0B-BEBE22260066/vendor/bundle/ruby/2.0.0/extensions/x86_64-darwin-14/2.0.0-static/nokogiri-1.6.6.2")
-$:.unshift File.expand_path("#{path}/../#{ruby_engine}/#{ruby_version}/gems/nokogiri-1.6.6.2/lib")
-$:.unshift File.expand_path("#{path}/../#{ruby_engine}/#{ruby_version}/gems/alfredo-0.1.2/lib")
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/mini_portile2-2.4.0/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/x86_64-darwin-18/2.4.0-static/nokogiri-1.10.3"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/nokogiri-1.10.3/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/alfredo-0.1.3/lib"
+$:.unshift "#{path}/"
